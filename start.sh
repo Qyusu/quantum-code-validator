bf63bc70c8061a6b99e5aba27ce21041f47f9b45
kenya-sk <30319295+kenya-sk@users.noreply.github.com>
2025-05-16 14:06:17 +0900

    fix: excute path

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "🔧 Running setup.py to fetch files from GCS..."
-python scripts/setup.py
+python -m scripts.setup
 
 echo "🚀 Starting main server..."
-python server.py
+python -m server.main
